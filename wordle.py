9791c9cc136a396ee62ae57603c8fd463c3d6b55
Dalton Hughes <dhughes3223@gmail.com>
2024-02-16 15:17:12 -0500

    Got a working ui for the game, have to play with layout and setting a limit on guesses and showing previously guessed words with correct letter placement

--- wordle.py
+++ wordle.py
@@ -7,48 +7,70 @@ checker = enchant.Dict('en_US')
 df = pd.read_csv('5_letter_words.csv')
 words = df['word'].tolist()
 
-word_entry = tk.StringVar()
-
-
-def getWord():
-    userEntry.get()
-
-def wordle():
-    while True:
-        userGuess = getWord()
-        if len(userGuess) != 5:
-            userGuess = getWord()
+def entryValid(word):
+    if len(word) != 5:
+        return False
+    return checker.check(word)
         
-        if checker.check(userGuess):
-            if userGuess.lower == word:
-                print("Correct!")
-                break
-            else:
-                count = 0
-                for i in userGuess:
-                    if i in word and userGuess.index(i) == word.index(i):
-                        print(f'\u001b[32m{i}\u001b[0m', end="")
-                    elif i in word and userGuess.index(i) != word.index(i):
-                        print(f'\u001b[33m{i}\u001b[0m', end="")
-                    else:
-                        print(f'\u001b[31m{i}\u001b[0m', end="")
-                print()
-
+def getWord():
+   return word_entry.get()
+   
+def wordle():
+    userGuess = word_entry.get()
+    if entryValid(userGuess):
+        if userGuess.lower == word:
+            print("Correct!")
         else:
-            print("Not a word!")
+            for i, label in zip(userGuess, char_labels):
+                if i in word and userGuess.index(i) == word.index(i):
+                    print(f'\u001b[32m{i}\u001b[0m', end="")
+                    # Color Green
+                    label.config(text = i, bg='green')
+                elif i in word and userGuess.index(i) != word.index(i):
+                    print(f'\u001b[33m{i}\u001b[0m', end="")
+                    # Color Yellow
+                    label.config(text = i, bg='yellow')
+                else:
+                    print(f'\u001b[31m{i}\u001b[0m', end="")
+                    # Color red
+                    label.config(text = i, bg='red')
+    else:
+        print("Invalid Entry")
 
 window = tk.Tk()
+window.rowconfigure([0, 1, 2], minsize=50)
+window.columnconfigure([0, 1, 2, 3, 4], minsize=50)
+
+char_labels = []
+char1 = tk.Label(text='')
+char_labels.append(char1)
+
+char2 = tk.Label(text='')
+char_labels.append(char2)
+
+char3 = tk.Label(text='')
+char_labels.append(char3)
+
+char4 = tk.Label(text='')
+char_labels.append(char4)
+
+char5 = tk.Label(text='')
+char_labels.append(char5)
+
+count = 0
+for labels in char_labels:
+    labels.grid(row=0, column=count)
+    count += 1
 
 label = tk.Label(text="Please Enter a Five Letter Word")
-userEntry = tk.Entry(width=50)
+word_entry = tk.StringVar()
+userEntry = tk.Entry(width=50, textvariable=word_entry)
 submitBtn = tk.Button(text='Submit', command=wordle)
-label.pack()
-userEntry.pack()
-submitBtn.pack()
-
+label.grid(row=1, column=2)
+userEntry.grid(row=2, column=2)
+submitBtn.grid(row=3, column=2)
 
 word = random.choice(words)
-
 
 print(word)
 
